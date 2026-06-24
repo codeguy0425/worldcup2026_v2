71ba2ffd5d7aee5ed47f16dc55e9ebe84fbb7ece
codeguy <andywong0425@gmail.com>
2026-06-24 09:40:13 +0800

    fix: use HashRouter for GH Pages SPA support

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { ThemeProvider } from './themes/ThemeProvider'
 import { Layout } from './components/Layout'
 import { HomePage } from './pages/Home'
@@ -10,7 +10,7 @@ import {
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <Layout>
           <Routes>
@@ -29,6 +29,6 @@ export default function App() {
           </Routes>
         </Layout>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
